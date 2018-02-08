bcde730d105a976a930a64aea249c5f9da0e606a
Nischitha <nischitha186@gmail.com>
2018-02-09 01:25:46 +0530

    File10 commit

---
+++ E2ESeleniumPractise/src/test/java/E2EPractise/ValidateNavigation.java
@@ -0,0 +1,51 @@
+package E2EPractise;
+
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pageObjects.LandingPage;
+import resources.Base;
+
+
+public class ValidateNavigation extends Base {
+
+	Logger log=LogManager.getLogger(ValidateNavigation.class.getName());
+	@BeforeTest
+	public void iniialize() throws IOException
+	{
+		driver=initializeDriver();
+		log.debug("Browser invoker");
+		driver.get(prop.getProperty("url"));
+		log.info("Navigated to url");
+	}
+	
+	@Test
+	public void basePageNavigation() throws IOException
+	{
+		
+		
+		LandingPage lPage=new LandingPage(driver);
+		String title=lPage.getTitle().getText();
+		
+		LandingPage lpage=new LandingPage(driver);
+		Assert.assertTrue(lpage.navBar().isDisplayed());
+		log.fatal("Incorrect Navigation Bar");
+		log.info("Navigation Bar Validated");
+
+	}
+	
+	@AfterTest
+	public void closeBrowser()
+	{
+		driver.close();
+		driver=null;
+	}
+	
+}
